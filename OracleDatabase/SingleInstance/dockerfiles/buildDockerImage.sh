745b953bf32437a0ebc0c5c5cee5bdbfa58b0c83
rmorrise <russell.morrisey+github@gmail.com>
2018-03-21 15:58:55 -0400

    Updated image name

--- OracleDatabase/SingleInstance/dockerfiles/buildDockerImage.sh
+++ OracleDatabase/SingleInstance/dockerfiles/buildDockerImage.sh
@@ -115,7 +115,7 @@ else
 fi
 
 # Oracle Database Image Name
-IMAGE_NAME="oracle/database:$VERSION-$EDITION"
+IMAGE_NAME="cscglobal/oracle/database-novol:$VERSION-$EDITION"
 
 # Go into version folder
 cd $VERSION
